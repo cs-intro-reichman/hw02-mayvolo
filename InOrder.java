5745f5d6317aef388ddaa8e0de528f1d052341cd
Mayvoloski <may.voloski@post.runi.ac.il>
2023-12-25 14:58:43 +0200

    inoreder check

--- InOrder.java
+++ InOrder.java
@@ -4,6 +4,13 @@
  */
 public class InOrder {
 	public static void main (String[] args) {
-		//// Write your code here
+		int x = (int) (Math.random() * 10);
+		System.out.print(x + " ");
+		int y = (int) (Math.random() * 10);
+		while(y >= x) {
+			System.out.print(y + " ");
+			x = y;
+			y = (int) (Math.random() * 10);
+		}
 	}
 }
