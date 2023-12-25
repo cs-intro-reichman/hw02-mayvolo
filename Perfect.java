51e6cd3c6d3b303c271335a3fca321e801af40fd
Mayvoloski <may.voloski@post.runi.ac.il>
2023-12-25 15:32:05 +0200

    edit Perfect

--- Perfect.java
+++ Perfect.java
@@ -3,6 +3,21 @@
  */
 public class Perfect {
 	public static void main (String[] args) {
-		//// Put your code here
+		int N = Integer.parseInt(args[0]);
+		int sum =1;
+		String s =  N + " is a perfect number since " + N + " = 1";
+		for(int i = 2; i < N; i++) {
+			if(N % i == 0) {
+				s = s + " + " + i;
+				sum = sum + i;
+			}
+		}
+		if (sum == N) {
+			System.out.println(s);
+		} else {
+			System.out.println(N + " is not a perfect number");
+		}
+		
+
 	}
 }
