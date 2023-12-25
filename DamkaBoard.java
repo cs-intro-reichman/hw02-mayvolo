6a3e12da482ce691eb5fe669136eda1f4cb8471c
Mayvoloski <may.voloski@post.runi.ac.il>
2023-12-25 17:37:56 +0200

    edit damka

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -3,6 +3,25 @@
  */
 public class DamkaBoard {
 	public static void main(String[] args) {
-		//// Put your code here
+		int n = Integer.parseInt(args[0]);
+		String s1 = "";
+		String s2 = " ";
+		for(int i=0; i<n; i++) {
+			s1 = s1 + "* ";
+			s2 = s2 + "* ";
+		}
+		if(n%2 == 0) {
+			for(int k=0; k<(n/2); k++) {
+				System.out.println(s1);
+				System.out.println(s2);
+			}
+		} else {
+			for(int j=0; j<((n-1)/2); j++) {
+				System.out.println(s1);
+				System.out.println(s2);
+			} 
+			System.out.println(s1);
+		}
+
 	}
 }
