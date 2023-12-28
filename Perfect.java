a481c48ca779f68d96f07ce9e2bd6f6ceebbe763
Mayvoloski <may.voloski@post.runi.ac.il>
2023-12-28 19:22:47 +0200

    edit perfect

--- Perfect.java
+++ Perfect.java
@@ -4,15 +4,12 @@
 public class Perfect {
 	public static void main (String[] args) {
 		int N = Integer.parseInt(args[0]);
-		int sum = 0;
+		int sum = 1;
 		String s =  N + " is a perfect number since " + N + " = 1";
-		for(int i = 1; i < N; i++) {
+		for(int i = 2; i < N; i++) {
 			if(N % i == 0) {
+				s = s + " + " + i;
 				sum = sum + i;
-				if(sum > 1) {
-					s = s + " + ";
-				}
-				s += i;
 			}
 		}
 		if (sum == N) {
